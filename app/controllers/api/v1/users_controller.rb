7d21636647eadff8efd70dcedadfd143e2467a03
Claudinei Perboni <cperbony@gmail.com>
2021-08-29 16:08:18 -0300

    Setup JWT gem

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::UsersController < ApplicationController
   
   # GET /users/1
   def show
-    render json: User.find(params[:id])
+      render json: User.find(params[:id])
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+      @user = User.new(user_params)
 
     if @user.save
       render json: @user, status: :created
@@ -35,10 +35,11 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def set_user
+    # @type [User]
     @user = User.find(params[:id])
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+   params.require(:user).permit(:email, :password)
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,27 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
+  before do
     @user = users(:one)
   end
-  
+
   test 'should show user' do
     get api_v1_user_url(@user), as: :json
     assert_response :success
     # Test to ensure response contains the correct email
-    json_response = JSON.parse(self.response.body)
+    json_response = JSON.parse(response.body)
     assert_equal @user.email, json_response['email']
   end
 
-  test "should create user" do
+  test 'should create user' do
     assert_difference('User.count') do
-    post api_v1_users_url, params: { user: { email:
-    'test@test.org', password: '123456' } }, as: :json
-  end
+      post api_v1_users_url, params: { user: { email:
+      'test@test.org', password: '123456' } }, as: :json
+    end
     assert_response :created
   end
-    
-  test "should not create user with taken email" do
+
+  test 'should not create user with taken email' do
     assert_no_difference('User.count') do
       post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
     end
@@ -30,7 +30,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should update user' do
     p 'USER', @user
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456'} }, as: :json
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
     assert_response :success
   end
 
@@ -45,5 +45,4 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :no_content
   end
-
 end
