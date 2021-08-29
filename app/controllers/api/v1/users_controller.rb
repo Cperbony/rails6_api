98212ae1b99eba6ca4c4942fc716518baefc0900
Claudinei Perboni <cperbony@gmail.com>
2021-08-28 22:32:44 -0300

    refactoring for updat and destroy methods test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+  
   # GET /users/1
   def show
     render json: User.find(params[:id])
