1c0ab3d1133601426d881b33e2623a56eabcecbf
Claudinei Perboni <cperbony@gmail.com>
2021-08-30 11:58:40 -0300

    Restrict actions for unauthorized users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  
+  before_action :check_owner, only: %i[update destroy]
+
   # GET /users/1
   def show
-      render json: User.find(params[:id])
+    render json: User.find(params[:id])
   end
 
   # POST /users
   def create
-      @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       render json: @user, status: :created
@@ -29,7 +30,7 @@ class Api::V1::UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
-    head 204
+    head :no_content
   end
 
   private
@@ -39,7 +40,11 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
+  end
+
   def user_params
-   params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
